34ee4b97fb08c71d259f48daf6e48c55e343f23d
MB <bergerm613@gmail.com>
2020-07-16 22:54:15 -0400

    started laying out basic architecture

--- sefariachallengereact/src/App.js
+++ sefariachallengereact/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import mapPlaceholder from './map.png';
 
-function App() {
-  return (
-    <div className="App">
-	<h1>Sefaria</h1>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      works: [],
+      currentEra: 'sinaiEra',
 
+      sinaiEra: [],
+      judgesEra: [],
+      KingsAndProphetsEra: [],
+      KnessetHagedolahEra: [],
+      zugotEra: [],
+      tannaimEra: [],
+      amoraimEra: [],
+      savoraimEra: [],
+      geonimEra: [],
+      rishonimEra: [],
+      achronimEra: [],
+    }
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <h2 className="garamond">Explore the Timeline of Jewish History</h2>
 
-
-
-
-
-
-
-
-
-
-
-    </div>
-  );
+        <img src={mapPlaceholder}/>
+        <div>*insert timeline here*</div>
+      </div>
+    )
+  }
 }
 
 export default App;
