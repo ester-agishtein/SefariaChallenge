6fd0257f94253656e7c7c5447866f457c1bb267c
ester-agishtein <eagishte@student.touro.edu>
2020-07-18 23:43:33 -0400

    Array of books created from API in componentDidMount

--- sefariachallengereact/src/App.js
+++ sefariachallengereact/src/App.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-import React from "react";
 import "./App.css";
 import ApiData from "./ApiData";
-function App() {
-  return (
-    <div className="App">
-      <h1>Sefaria</h1>
-      <ApiData />
-    </div>
-  );
-=======
-import React, {Component} from 'react';
-import './App.css';
-import mapPlaceholder from './map.png';
+import mapPlaceholder from "./map.png";
+import React, { Component } from "react";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       works: [],
-      currentEra: 'sinaiEra',
+      currentEra: "sinaiEra",
 
       sinaiEra: [],
       judgesEra: [],
@@ -31,21 +20,20 @@ class App extends Component {
       savoraimEra: [],
       geonimEra: [],
       rishonimEra: [],
-      achronimEra: [],
-    }
+      achronimEra: []
+    };
   }
 
   render() {
     return (
       <div className="App">
         <h2 className="garamond">Explore the Timeline of Jewish History</h2>
-
-        <img src={mapPlaceholder}/>
+        <img src={mapPlaceholder} />
+        <ApiData />
         <div>*insert timeline here*</div>
       </div>
-    )
+    );
   }
->>>>>>> 34ee4b97fb08c71d259f48daf6e48c55e343f23d
 }
 
 export default App;
